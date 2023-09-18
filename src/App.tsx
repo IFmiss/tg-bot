36c9aa462ed04ef2448c543f9f289bb6a0a9086f
ifmiss <185098535@qq.com>
2023-09-18 10:18:11 -0400

    update: test auth

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import './App.css';
+import React, { useEffect } from 'react';
+
+Telegram.WebApp.ready();
+
+function App() {
+  useEffect(() => {
+    Telegram.WebApp.MainButton.color = '#57ca3a';
+    Telegram.WebApp.MainButton.textColor = '#0d1e08';
+    Telegram.WebApp.MainButton.text = 'Continue';
+    Telegram.WebApp.MainButton.onClick(() => {
+      Telegram.WebApp.showConfirm('Are you confirm', () => {
+        Telegram.WebApp.close()
+      })
+    })
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/theme.js
@@ -0,0 +1,6 @@
+export const theme = {
+  bg_color: '#f1f1f1',
+  link_color: '#57ca3a',
+  button_color: '#57ca3a',
+  button_text_color: '#0d1e08',
+}
